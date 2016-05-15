b05298b1a7c3df9b03470525a0b64a9c33cdca2e
Jakob Erzar <jakob.erzar@gmail.com>
2016-05-15 05:22:00 +0000

    Dodan mongo

--- server.js
+++ server.js
@@ -3,16 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var multer = require('multer');
-
-app.use('/', express.static(__dirname + '/public_html'));
-app.get('/', function (req, res) {
-  res.sendfile('public_html/index.html');
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended:true
-}));
+var monk = require('monk');
+var db = monk('localhost:27017/baza1');
 
 var entries=[{
     latitude: 46,
@@ -27,6 +19,45 @@ var entries=[{
     imglink: '/uploads/klasika.jpg'
   }
 ];
+
+
+/*
+
+function dodajGaNot(vnos, callback) {
+  entries.push(entry);
+  MongoClient.connect("mongodb://localhost:27017/baza1", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+      db.createCollection('entries', function(err, collection) {
+        if (err) {console.error(err) }
+        else {
+          entries = collection;
+          collection.insert(vnos);
+        }
+        callback(true);
+      });
+    } else { callback(false) }
+    
+  });
+}*/
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+
+app.use('/', express.static(__dirname + '/public_html'));
+app.get('/', function (req, res) {
+  res.sendfile('public_html/index.html');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
 
 // Upload brez slike
 app.post('/api/v1/upload', function(req, res) {
@@ -61,14 +92,20 @@ app.post('/api/v1/upload/photo', upload.single('image'), function(req, res, next
     imglink: '/uploads/' + req.body.id + ".jpg",
     time: req.body.time
   };
-  entries.push(entry);
+  var db = req.db;
+  var collection = db.get('entries');
+  collection.insert(entry);
   res.end("gg uploaded lol that was ez!")
 });
 
 
 app.get('/api/v1/entries', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(entries));
+  var db = req.db;
+  var collection = db.get('entries');
+  collection.find({},{},function(e,docs){
+     res.send(JSON.stringify(docs));   
+  });
 });
 
 app.listen(8080, function () {
