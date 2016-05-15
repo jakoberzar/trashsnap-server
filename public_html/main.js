96444854b9ba8253f062c1b6d73ebdeb2f92bd6f
Robert Dovžan <rdovzan@gmail.com>
2016-05-15 01:08:17 +0000

    Manjši popravek

--- public_html/main.js
+++ public_html/main.js
@@ -12,26 +12,6 @@ var entries = [];
 
 $.get("/api/v1/entries", function(data) {
     var marker = [];
-    for(var i = 0; i < data.length; i++) {
-        var trenutni = data[i];
-        var mk = {
-            position: {lat: trenutni.latitude, lng: trenutni.longitude},
-            map: map,
-            title: 'World! ' + [i]
-        };
-        var mark = new google.maps.Marker(mk)
-        marker.push(mark);
-        //console.log(marker[i].title);
-        //var naslov = marker[i].title;
-        
-        mark.addListener('click', function() {
-            $("#slika").html("<img class='slika1' src='" + trenutni.imglink + "' >");
-            $("#naslov").html("FUCK YOU " + mk.text);
-            $("#datum").html("Datum");
-            $("#opis").html("Opis");
-        });
-    }
-    
     data.forEach(function(f) {
         var trenutni = f;
         var mk = {
